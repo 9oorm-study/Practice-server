62c282ef9b20c668f1759085be7d624d3bbd79f0
Jeongmo Seo <wjdah1402@gmail.com>
2025-04-14 13:12:08 +0900

    :rocket: chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/microservices/demo/practiceserver/PracticeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.practiceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
